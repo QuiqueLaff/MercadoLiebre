4bb8aaadd9ca6f9121916bae150d2cb0e1498b85
QuiqueLaff <enriquelaffranconi@gmail.com>
2021-09-28 10:37:40 -0300

    Mercado Liebre

---
+++ Desktop/Mercado Liebre Entregable/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando en el 3000');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/crear-producto', (req,res)=>{
+    res.sendFile(__dirname + '/views/crear-producto.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login1.html');
+});
